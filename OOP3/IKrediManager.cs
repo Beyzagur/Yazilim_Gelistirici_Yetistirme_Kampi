614e6ac0e51f1855e773ccdcf86ffe25793b2883
Beyzagur <58330314+Beyzagur@users.noreply.github.com>
2021-02-20 02:40:47 +0300

    Yazılım Geliştirici Yetiştirme Kampı 5. gün ders sonu

---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //okunurluğunu arttırmak için I harfi ile başlanır.
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BirseyYap();
+    }
+}
