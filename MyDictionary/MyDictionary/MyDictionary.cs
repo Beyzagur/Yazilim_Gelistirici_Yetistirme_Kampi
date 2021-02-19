8bbdb2c1b83f0fa0a5f595401bfba93b95348349
Beyzagur <58330314+Beyzagur@users.noreply.github.com>
2021-02-19 23:57:03 +0300

    MyDictionary project is completed.

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        List<K> key;
+        List<V> value;
+
+        public MyDictionary()
+        {
+            key = new List<K>();
+            value = new List<V>();
+        }
+
+        public void Add(K new_key, V new_value)
+        {
+            if (!key.Contains(new_key))
+            {
+                key.Add(new_key);
+                value.Add(new_value);
+            }
+        }
+
+        public bool ContainsKey(K new_key)
+        {
+            if (key.Contains(new_key))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ContainsValue(V new_value)
+        {
+            if (value.Contains(new_value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Key(K new_key)
+        {
+            Console.WriteLine(value[key.IndexOf(new_key)]);
+        }
+
+        public void Value(V new_value)
+        {
+            Console.WriteLine(key[value.IndexOf(new_value)]);
+        }
+
+    }
+}
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(2018510035, "Beyza");
+            myDictionary.Add(1234567890, "name1");
+            myDictionary.Add(1122334455, "name2");
+            myDictionary.Add(1472583690, "name3");
+            myDictionary.Add(1594872630, "name4");
+
+            myDictionary.Key(2018510035);
+            myDictionary.Value("name3");
+            Console.WriteLine(myDictionary.ContainsKey(0126534789));
+            Console.WriteLine(myDictionary.ContainsValue("name2"));
+            
+        }
+    }
+}
