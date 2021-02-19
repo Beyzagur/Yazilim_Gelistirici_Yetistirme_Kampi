00ac1119511ffe810ddf9cf4fa53ee45081e0b29
Beyzagur <58330314+Beyzagur@users.noreply.github.com>
2021-02-20 02:45:58 +0300

    ClassMethodDemo assignment completed.

---
+++ ClassMethodDemo/ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine("\n"+customer.CustomerName+" "+customer.CustomerSurname+" customer has been added !\n");
+        }
+
+        public void Delete(Customer deleteCustomer)
+        {
+            foreach (Customer customer in customers)
+            {
+                if(deleteCustomer.CustomerName.Equals(customer.CustomerName) & deleteCustomer.CustomerSurname.Equals(customer.CustomerSurname))
+                {
+                    customers.Remove(customer);
+                    Console.WriteLine("\n" + deleteCustomer.CustomerName + " " + deleteCustomer.CustomerSurname + " customer has been deleted !!!\n");
+                   
+                }
+                else
+                {
+                    Console.WriteLine("\nThere is no customer you want to delete !!!\n");
+                }
+            }
+            if (customers.Count == 0)
+            {
+                Console.WriteLine("\nThere is no customer you want to delete !!!\n");
+
+            }
+
+        }
+
+        public void List()
+        {
+            if (customers.Count > 0)
+            {
+                Console.WriteLine("\n----------List of Customers Information----------");
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine(customers.IndexOf(customer)+1+")"+customer.CustomerName + " " + customer.CustomerSurname + "   \nCustomer Age: "+ customer.CustomerAge);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nDoes not have a customer !!!\n");
+            }
+           
+        }
+    }
+}
---
+++ ClassMethodDemo/ClassMethodDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            bool firstRun = true;
+            bool secondRun = true;
+
+            Console.WriteLine("\tWelcome to the Bank ");
+
+            while (run)
+            {
+                try
+                {
+                    CustomerManager cm = new CustomerManager();
+                    while (firstRun)
+                    {
+                        Console.Write("1 Add customer\n2 Delete customer\n3 List customers' information \n4 Exit \nSelect the action you want to carry out: ");
+                        int choose = Convert.ToInt32(Console.ReadLine());
+
+                        switch (choose)
+                        {
+                            case 1:
+                                while (secondRun)
+                                {
+                                    Console.Write("Enter the number of customers to add: ");
+                                    try
+                                    {
+                                        int count = Convert.ToInt32(Console.ReadLine());
+                                        for (int i = 1; i <= count; i++)
+                                        {
+                                            Customer customer = new Customer();
+                                            Console.Write("\nPlease write your name: ");
+                                            customer.CustomerName = Console.ReadLine();
+                                            Console.Write("Please write your surname: ");
+                                            customer.CustomerSurname = Console.ReadLine();
+                                            Console.Write("Please write your age: ");
+                                            customer.CustomerAge = Convert.ToInt32(Console.ReadLine());
+
+                                            cm.Add(customer);
+                                            secondRun = false;
+
+                                        }
+                                    }
+                                    catch (Exception)
+                                    {
+                                        Console.WriteLine("\n***You entered the wrong format! \n");
+                                    }
+                                }
+                                break;
+                            case 2:
+                                Customer deleteCustomer = new Customer();
+                                Console.Write("\nPlease write your name: ");
+                                deleteCustomer.CustomerName = Console.ReadLine();
+                                Console.Write("Please write your surname: ");
+                                deleteCustomer.CustomerSurname = Console.ReadLine();
+
+                                cm.Delete(deleteCustomer);
+
+                                break;
+                            case 3:
+                                cm.List();
+                                break;
+                            case 4:
+                                firstRun = false;
+                                Console.WriteLine("\nHave a nice day");
+                                break;
+                            default:
+                                Console.WriteLine("\nYou did not choose one of these options !!!\n ");
+                                break;
+                        }
+                    }
+                    run = false;
+
+                }
+                catch
+                {
+                    Console.WriteLine("\n***You entered the wrong format!\n");
+                }
+            }
+            
+        }
+    }
+}
